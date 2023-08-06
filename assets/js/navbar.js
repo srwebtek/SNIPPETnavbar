2aa4afd9b0765c18a5f6f864671a26da086ca413
srwebtek <srwebtek@gmail.com>
2023-08-06 23:25:46 +1000

    got the json to work. messy but productive

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,3 +1,53 @@
+// const dropDownContent = `
+//   <div class="footerMenu">
+//     <ul id="footerMainNav" class="footerMainNav"></ul>
+//     <div class="footerHorizontalLine"></div>
+//     <ul id="footerSocialNav"></ul>
+//     <div class="footerHorizontalLine"></div>
+//     <ul id="footerPoliciesNav"></ul>
+//   </div>
+//   <div class="footerCopyright" aria-hidden="true">
+//     <a href="https://srwebtek.github.io/">Copyright srwebtek &copy 2023</a>
+//   </div>
+// `;
+// document.getElementById("++++++++").innerHTML = dropDownContent;
+
+// -----------------------------------------
+//  navigation menu
+// -----------------------------------------
+
+
+
+fetch("./assets/json/navbar.json")
+  .then(function (resp) {
+    return resp.json();
+  })
+  .then(function (data) {
+    for (var i = 0; i < data.mainNavigation[0].pages.length; i++) {
+      console.log(data.mainNavigation[0].pages[i].title);
+
+  }});
+
+// var mainNavOutput = "";
+
+// fetch("../assets/json/navbar.json")
+//   .then(function (resp) {
+//     return resp.json();
+//   })
+//   .then(function (data) {
+//     for (var i = 0; i < data.main.length; i++) {
+//       mainNavOutput +=
+//         '<li><a href="' +
+//         data.main[i].link +
+//         '">' +
+//         data.main[i].title +
+//         "</a></li>";
+//     }
+
+//     document.getElementById("footerMainNav").innerHTML = mainNavOutput;
+
+//   });
+
 // -----------------------------------------------------
 // This controls the dropdown menubar event listener
 // -----------------------------------------------------
